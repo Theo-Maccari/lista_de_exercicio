ef673a5da555d9c071e9b530ac9dd000c7d1581a
theo-maccari <theo_maccari@etudante.sesisenai.org.br>
2026-07-09 09:01:05 -0300

    add - exercício 8 iniciado

--- exercício_06.php
+++ exercício_06.php
@@ -2,7 +2,20 @@
 
  function converterTemperatura($temperatura, $origem, $destino) {
 
+$c = 0;
+$f = 0;
+$k = 0;
 
+case 'C':
+
+    $celsius = $valor;
+            break;
+        case 'F':
+            $celsius = ($valor - 32) * 5 / 9;
+            break;
+        case 'K':
+            $celsius = $valor - 273.15;
+            break;
 
  }
 
--- exercício_07.php
+++ exercício_07.php
@@ -1,7 +1,22 @@
 <?php
 
 function calcularDesconto($valor, $desconto) {
- 
+
+if ($valor <= 100){
+$desconto = 0;
+
+}else{ if ($valor <= 500){
+$desconto = 10;
+
+}else{ if ($valor <= 1000){
+$desconto = 20;
+
+}else{ $desconto <= 30}
+    }
 }
+
+}
+
+$valor
 
 ?>
